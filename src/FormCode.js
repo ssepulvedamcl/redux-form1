0312dd493f93e293aea12858a424dcda0bcf1cad
Sergio Sepulveda <sergio.sepulveda@dev-solutions.cl>
2019-10-28 19:22:55 -0300

    Version Final pasos 1- 5 redux-form

--- src/FormCode.js
+++ src/FormCode.js
@@ -1,6 +1,27 @@
 import React from 'react';
 import { Field, reduxForm } from 'redux-form';
 
+
+const validate = values => {
+    const errors = {}
+    if (!values.firstName) {
+      errors.firstName = 'Required'
+    } else if (values.firstName.length < 2) {
+      errors.firstName = 'Minimum be 2 characters or more'
+    }
+    if (!values.email) {
+      errors.email = 'Required'
+    } else if (!/^[A-Z0-9._%+-]+@[A-Z0-9.-]+\.[A-Z]{2,4}$/i.test(values.email)) {
+      errors.email = 'Invalid email address'
+    }
+    if (!values.lastName) {
+        errors.lastName = 'Required'
+      } else if (values.lastName.length < 2) {
+        errors.lastName = 'Minimum be 2 characters or more'
+      }
+    return errors
+  }
+  
 const renderField = ({ input, label, type, meta: { touched, error, warning }}) => (
     <div>
         <label className="control-label">{label}</label>
@@ -32,8 +53,11 @@ let FormCode = props => {
     )
 }
 
+
+
 FormCode = reduxForm({
-    form:'contact'
+    form:'contact',
+    validate,
 })(FormCode);
 
 export default FormCode;
